9edfdcacba029034076ad62b3d3d15d6dff50486
dwahomewaweru <dwahome.waweru@strathmore.edu>
2022-07-25 18:49:36 +0300

    Updated Spring Boot Framework Real Estate Co.

--- realestateco/src/main/java/com/apps/realestateco/Model/Agent.java
+++ realestateco/src/main/java/com/apps/realestateco/Model/Agent.java
@@ -1,2 +1,77 @@
-package com.apps.realestateco.Model;public class Agent {
+package com.apps.realestateco.Model;
+
+import org.hibernate.type.descriptor.sql.NVarcharTypeDescriptor;
+import org.hibernate.type.descriptor.sql.VarcharTypeDescriptor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "AGENT")
+public class Agent {
+
+    @Id
+    @Column(name = "AGENT_ID")
+    private Long agentId;
+    @Column(name = "AGENT_NAME")
+    private String agentName;
+    @Column(name = "PHONE_NUMBER")
+    private int phoneNumber;
+    @Column(name = "EMAIL")
+    private VarcharTypeDescriptor email;
+
+    public Agent(Long agentId, String agentName, int phoneNumber, VarcharTypeDescriptor email) {
+        this.agentId = agentId;
+        this.agentName = agentName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Agent() {
+
+    }
+
+    public Long getAgentId() {
+        return agentId;
+    }
+
+    public void setAgentId(Long agentId) {
+        this.agentId = agentId;
+    }
+
+    public String getAgentName() {
+        return agentName;
+    }
+
+    public void setAgentName(String agentName) {
+        this.agentName = agentName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public VarcharTypeDescriptor getEmail() {
+        return email;
+    }
+
+    public void setEmail(VarcharTypeDescriptor email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Agent{" +
+                "agentId=" + agentId +
+                ", agentName='" + agentName + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", email=" + email +
+                '}';
+    }
 }
--- realestateco/src/main/java/com/apps/realestateco/Model/Property.java
+++ realestateco/src/main/java/com/apps/realestateco/Model/Property.java
@@ -1,2 +1,95 @@
-package com.apps.realestateco.Model;public class Property {
+package com.apps.realestateco.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "PROPERTY")
+public class Property {
+
+    @Id
+    @Column(name = "PROPERTY_ID")
+    private Long propertyId;
+    @Column(name = "PROPERTY_TITLE")
+    private String title;
+    @Column(name = "PROPERTY_TYPE")
+    private String type;
+    @ManyToOne
+    @JoinColumn(name = "AGENT_ID")
+    private Agent agent;
+    @Column( name = "PROPERTY_LOCATION")
+    private String location;
+    @Column(name = "PROPERTY_AREASIZE")
+    private int areaSize;
+    @Column(name = "PROPERTY_PRICE")
+    private int price;
+
+    public Property(Long propertyId, String title, String type, Agent agent, String location, int areaSize, int price) {
+        this.propertyId = propertyId;
+        this.title = title;
+        this.type = type;
+        this.agent = agent;
+        this.location = location;
+        this.areaSize = areaSize;
+        this.price = price;
+    }
+
+    public Property() {
+
+    }
+
+    public Long getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(Long propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Agent getAgent() {
+        return agent;
+    }
+
+    public void setAgent(Agent agent) {
+        this.agent = agent;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getAreaSize() {
+        return areaSize;
+    }
+
+    public void setAreaSize(int areaSize) {
+        this.areaSize = areaSize;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
